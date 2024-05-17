e98786668afe22fa54ae7edf3a4b983991c65d63
dm <88588404+druxvh@users.noreply.github.com>
2024-05-17 19:32:50 +0000

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,41 @@
+import {
+  Hero,
+  Footer,
+  CustomerReviews,
+  SpecialOffers,
+  SuperQuality,
+  Subscribe,
+  PopularProducts,
+  Services,
+} from "./sections";
+import Nav from "./components/Nav";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <main className="relative">
+      <Nav/>
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
 }
