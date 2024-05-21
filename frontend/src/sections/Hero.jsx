a7f1a87be950e8dc419b3b895c3d01d1e8e2d9b8
dm <88588404+druxvh@users.noreply.github.com>
2024-05-21 19:18:55 +0000

    added special offers section and bugs fixes

--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-500 p-2"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container  p-2"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
@@ -49,7 +49,7 @@ const Hero = () => {
         />
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe) => (
-            <div key={shoe}>
+            <div key={shoe.bigShoe}>
               <ShoeCard 
               imgURL= {shoe}
               changeBigShoeImage={(shoe)=>{setbigShoeImage(shoe)}}
--- frontend/src/sections/SpecialOffers.jsx
+++ frontend/src/sections/SpecialOffers.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Button from '../components/Button'
+import { offer } from '../assets/images'
+import { arrowRight } from '../assets/icons'
 
 const SpecialOffers = () => {
   return (
-    <div>SpecialOffers</div>
+    <section className="flex flex-wrap justify-center items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer} alt="offer" width={773} height={687} className='object-contain w-full' />
+      </div>
+      <div className="flex flex-1 flex-col">
+      <h2 className="font-palanquin text-4xl capitalize font-bold max-w-lg">
+          
+          <span className="text-coral-red "> Special </span> Offers
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+          laboriosam quibusdam, tempore quo, eius provident officia maxime
+          doloribus tempora optio blanditiis porro quia ipsum corporis.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati eveniet itaque magni dolorem aliquid perferendis iure et voluptate.
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop Now" iconURL={arrowRight} />
+          <Button label="Learn More" backgroundColor = "bg-white" borderColor = "border-slate-gray" textColor = "text-slate-gray" />
+        </div>
+      </div>
+    </section>
   )
 }
 
